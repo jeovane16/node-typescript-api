593779d62596a183b7f4f71af4025bebbaf0de1a
Jeovane Araujo <jeovane.araujo@pixter.com.br>
2022-06-17 15:28:40 -0300

    add moongose hooks and validations

---
+++ src/controller/index.ts
@@ -0,0 +1,33 @@
+import { Response } from 'express';
+import mongoose from 'mongoose';
+import { CUSTOM_VALIDATION } from '@src/models/user';
+
+export abstract class BaseController {
+  protected sendCreatedUpdateErrorResponse(
+    res: Response,
+    error: unknown
+  ): void {
+    if (error instanceof mongoose.Error.ValidationError) {
+      const clientErrors = this.handleClientErrors(error);
+      res.status(clientErrors.code).send({ ...clientErrors });
+    } else {
+      res.status(500).send({ code: 500, error: 'Something went wrong!' });
+    }
+  }
+
+  private handleClientErrors(error: mongoose.Error.ValidationError): {
+    code: number;
+    error: string;
+  } {
+    const duplicatedKindErrors = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+    if (duplicatedKindErrors.length) {
+      return {
+        code: 409,
+        error: error.message,
+      };
+    }
+    return { code: 422, error: error.message };
+  }
+}
