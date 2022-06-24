fae559e8e6663f0de5c2f903a2475586ccdae741
Jeovane Araujo <jeovane.araujo@pixter.com.br>
2022-06-23 23:05:11 -0300

    add authService and bcrypt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Model } from 'mongoose';
+import AuthService from '@src/services/auth';
 
 export interface User {
   _id?: string;
@@ -26,7 +27,7 @@ const schema = new mongoose.Schema(
   {
     toJSON: {
       transform: (_, ret): void => {
-        ret.id = ret.id;
+        ret.id = ret._id;
         delete ret._id;
         delete ret._v;
       },
@@ -34,9 +35,25 @@ const schema = new mongoose.Schema(
   }
 );
 
-schema.path('email').validate(async (email: string)=> {
-  const emailCount = await mongoose.models.User.countDocuments({ email });
-  return !emailCount;
-}, 'already exists in the database.', CUSTOM_VALIDATION.DUPLICATED);
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    this.password = await AuthService.hashPassword(this.password);
+  } catch (error) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
